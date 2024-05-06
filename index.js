1f15fda5bffd548aa3418b39e42875b3bdfab682
santerilukka <156800715+santerilukka@users.noreply.github.com>
2024-05-06 20:53:29 +0300

    3.5: puhelinluettelon backend step5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
         id: 1,
@@ -54,6 +56,20 @@ app.delete('/api/persons/:id', (request, response) => {
   
     response.status(204).end()
 })
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    const person = {
+        id: Math.floor(Math.random() * 1000000),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+  })
 
 const PORT = 3001
 app.listen(PORT)
