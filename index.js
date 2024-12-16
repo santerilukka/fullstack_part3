8e42e5ffc17f29d229c8d26439fa1548de0014ce
santerilukka <156800715+santerilukka@users.noreply.github.com>
2024-12-16 18:19:19 +0200

    3.16: puhelinluettelo ja tietokanta, step4

--- index.js
+++ index.js
@@ -42,15 +42,12 @@ app.get('/api/persons/:id', (request, response) => {
     }
   })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
     .then(result => {
         response.status(204).end()
     })
-    .catch(error => {
-        console.log(error)
-        response.status(400).send({ error: 'malformatted id' })
-      })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -66,6 +63,18 @@ app.post('/api/persons', (request, response) => {
         response.json(savedPerson)
     })
   })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+  
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
