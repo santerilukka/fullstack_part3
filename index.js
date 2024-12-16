6f73cfe587a818ce7194a1c2aa508151d87374d4
santerilukka <156800715+santerilukka@users.noreply.github.com>
2024-12-16 17:48:11 +0200

    3.14: puhelinluettelo ja tietokanta, step2

--- index.js
+++ index.js
@@ -52,23 +52,15 @@ app.delete('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request, response) => {
     const body = request.body
 
-    if (!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'Nimi tai numero puuttuu'})
-    } else if (persons.some(person => person.name === body.name)) {
-        return response.status(400).json({
-            error: 'Nimen on oltava uniikki'})
-    }
-
-    const person = {
-        id: Math.floor(Math.random() * 1000000),
+    const person = new Person({
         name: body.name,
-        number: body.number
-    }
+        number: body.number,
+    })
 
-    persons = persons.concat(person)
-
-    response.json(person)
+    person.save().then(savedPerson => {
+        console.log(savedPerson)
+        response.json(savedPerson)
+    })
   })
 
 const PORT = process.env.PORT
