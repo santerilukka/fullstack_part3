46d47692c2b7882322fcca04b6190caae2cfd8e2
santerilukka <156800715+santerilukka@users.noreply.github.com>
2024-12-16 15:43:11 +0200

    3.13: puhelinluettelo ja tietokanta, step1

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+
+const Person = require('./models/person')
 
 app.use(cors())
 app.use(express.json())
@@ -10,32 +13,14 @@ app.use(express.static('dist'))
 app.use(morgan(':method :url :status :response-time ms - :postData'));
 morgan.token('postData', (req) => JSON.stringify(req.body));
 
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-    },
-]
+
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
-})
+    Person.find({}).then(persons => {
+      console.log(persons)
+      response.json(persons)
+    })
+  })
 
 app.get('/info', (request, response) => {
     const amount = String(persons.length)
@@ -86,6 +71,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
   })
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
