8d91f097e91e6d7d70cfed509afa7384cc58e011
santerilukka <156800715+santerilukka@users.noreply.github.com>
2024-12-17 20:57:10 +0200

    3.20*: puhelinluettelo ja tietokanta, step8

--- models/person.js
+++ models/person.js
@@ -21,7 +21,17 @@ const personSchema = new mongoose.Schema({
     minlength: 3,
     required: true
   },
-  number: String,
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d{5,}$/.test(v)
+    },
+    message: props => `${props.value} is not a valid phone number! Make sure the phone number is in the format 12-345678 or 123-45678`
+    },
+    required: [true, 'User phone number required']
+  }
 })
 
 personSchema.set('toJSON', {
